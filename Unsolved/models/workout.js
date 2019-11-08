7a4ef5c9f70fe9d862d619e4f22b3e6c2846aded
anthony soon <anthonychansoon@gmail.com>
2019-11-07 21:52:30 -0800

    html routes and workout models

--- Unsolved/models/workout.js
+++ Unsolved/models/workout.js
@@ -3,6 +3,32 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const workoutSchema = new Schema(
+  {
+    day: {
+      type: Date
+    },
+    exercises: [
+      {
+        type: {
+          type: String
+        },
+        name: {
+          type: String
+        },
+        duration: {
+          type: Number
+        },
+        weight: {
+          type: Number
+        },
+        reps: {
+          type: Number
+        },
+        sets: {
+          type: Number
+        }
+      }
+    ]
   },
   {
     toJSON: {
@@ -13,7 +39,7 @@ const workoutSchema = new Schema(
 );
 
 // adds a dynamically-created property to schema
-workoutSchema.virtual("totalDuration").get(function() {
+workoutSchema.virtual("totalDuration").get(function () {
   // "reduce" array of exercises down to just the sum of their durations
   return this.exercises.reduce((total, exercise) => {
     return total + exercise.duration;
--- Unsolved/routes/api.js
+++ Unsolved/routes/api.js
@@ -1,5 +1,24 @@
 const router = require("express").Router();
 const Workout = require("../models/workout.js");
 
+//const db = mongojs("workout", ["workout"]);
+
+
+// router.get("/api/workouts", function(req, res){
+//     //console.log("hello");
+//     Workout.find({}, (err, data) => {
+//         if (err) {
+//             console.log(err);
+//         } else {
+//             //console.log(data);
+//             res.json(data);
+//         }
+//     })
+// })
+
+router.post("/api/workouts", function (req, res) {
+    
+})
+
 
 module.exports = router;
