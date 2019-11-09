9ed5fd4e406e915ff3eac97644000a5004cb4a2f
anthony soon <anthonychansoon@gmail.com>
2019-11-08 23:42:35 -0800

    herok

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
 
-//const PORT = 3000;
+const PORT = 3000;
 
 const app = express();
 
@@ -12,6 +12,24 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use(express.static("public"));
+
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://<dbuser>:<dbpassword>@ds241268.mlab.com:41268/heroku_g67ngjt4";
+
+mongoose.connect(MONGODB_URI, {
+  useNewUrlParser: true,
+  useFindAndModify: false
+});
+
+// routes
+app.use(require("./routes/api.js"));
+app.use(require("./routes/view.js"));
+
+
+app.listen(process.env.PORT || PORT, () => {
+  console.log(`App running on port ${PORT}!`);
+});
+
+
 
 // mongoose.connect("mongodb://localhost/workout", {
 //   useNewUrlParser: true,
@@ -27,21 +45,6 @@ app.use(express.static("public"));
 //     useMongoClient: true
 //   }
 // );
-
-var MONGODB_URI = process.env.MONGODB_URI || "mongodb://user1:password1@ds241268.mlab.com:41268/heroku_g67ngjt4";
-
-mongoose.connect(MONGODB_URI, {
-  useNewUrlParser: true,
-  useFindAndModify: false
-});
-
-// routes
-app.use(require("./routes/api.js"));
-app.use(require("./routes/view.js"));
-
-app.listen(process.env.PORT || PORT, () => {
-  console.log(`App running on port ${PORT}!`);
-});
 
 
 
