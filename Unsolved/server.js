28be33b5332775a5e6a5e8c68780c8c605b21cda
anthony soon <anthonychansoon@gmail.com>
2019-11-08 22:36:29 -0800

    heroku

--- Unsolved/server.js
+++ Unsolved/server.js
@@ -13,10 +13,14 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/workout", {
-  useNewUrlParser: true,
-  useFindAndModify: false
-});
+// mongoose.connect("mongodb://localhost/workout", {
+//   useNewUrlParser: true,
+//   useFindAndModify: false
+// });
+
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+
+mongoose.connect(MONGODB_URI);
 
 // routes
 app.use(require("./routes/api.js"));
@@ -31,17 +35,3 @@ app.listen(PORT, () => {
 
 
 
-
-// router.put("/api/workouts/:id", ({ body, params }, res) => {
-//     Workout.findByIdAndUpdate(
-//         params.id,
-//         { $push: { exercises: body } },
-//         { new: true, runValidators: true }
-//     )
-//         .then(dbWorkout => {
-//             res.json(dbWorkout);
-//         })
-//         .catch(err => {
-//             res.json(err);
-//         });
-// });
